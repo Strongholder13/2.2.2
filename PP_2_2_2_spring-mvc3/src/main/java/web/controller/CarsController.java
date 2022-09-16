d9d8477d49d18a9361bdd33fd3e2615964040899
Strongholder13 <108480254+Strongholder13@users.noreply.github.com>
2022-09-16 21:04:05 +0300

    Update CarsController.java

--- PP_2_2_2_spring-mvc3/src/main/java/web/controller/CarsController.java
+++ PP_2_2_2_spring-mvc3/src/main/java/web/controller/CarsController.java
@@ -1,11 +1,9 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.dao.CarDAO;
@@ -15,15 +13,15 @@ import web.dao.CarDAO;
 public class CarsController {
 
     private final CarDAO carDAO;
+
     @Autowired
     public CarsController(CarDAO carDAO) {
         this.carDAO = carDAO;
     }
 
-@GetMapping
-    public String list(@RequestParam(value = "count", required = false) Integer count, Model model){
-        if (count == null){count = 5;}
+    @GetMapping
+    public String list(@RequestParam(value = "count", required = false) Integer count, Model model) {
         model.addAttribute("cars", carDAO.show(count));
-return "/cars";
+        return "/cars";
     }
 }
